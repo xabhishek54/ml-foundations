520e1dccf6fd3012f056964d1a1dd3895bb227b5
xabhishek54 <abhishek54yt@gmail.com>
2026-07-29 23:12:43 +0530

    indexing

---
+++ numpy-basics/Indexing-Slicing/indexing=access.py
@@ -0,0 +1,9 @@
+#Indexing is used to access a particular element
+#There is positive and negative indexing
+#If we use index no beyond capactiy then IndexError occurs
+
+import numpy as np
+arr=np.array([10,25,43,67,34,19])
+print(arr[2])
+print(arr[2])
+print(arr[-1])
